533fc34160bee2e811fca6ba0e31240bedd6112f
dev-ming <ruaaaa@ajou.ac.kr>
2023-12-11 02:50:52 +0900

    :bento: add christmas tree asset

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import christmasTree from "./christmas_tree.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <p style={{ fontWeight: "bold" }}>Merry Christmas!</p>
+        <img src={christmasTree} style={{ height: "500px" }} />
       </header>
     </div>
   );
